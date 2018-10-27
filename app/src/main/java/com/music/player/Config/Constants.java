a6c1b36ce2c8b3a8933a6c28fb4ed04654926f3e
root <874189630@qq.com>
2018-10-27 22:37:16 +0800

    项目初始化

---
+++ app/src/main/java/com/music/player/Config/Constants.java
@@ -0,0 +1,16 @@
+package com.music.player.Config;
+
+
+public class Constants {
+    public final static String CHANGE_MUSIC_SONG = "change.music.song";
+
+    public final static String CHANGE_MUSIC_START_PAUSE = "change.music.start.pause";
+
+    public final static String CHANGE_MUSIC_UP_MUSIC_DATA = "change.music.up_music_data";
+
+    public final static String CHANGE_PLAYED = "change.music.played";
+
+    public final static String CHANGE_ALL_LIST = "change.music.all.list";
+
+    public final static String CHANGE_COLLECT_LIST = "change.music.collect.list";
+}
---
+++ app/src/main/java/com/music/player/bean/Song.java
@@ -0,0 +1,27 @@
+package com.music.player.bean;
+
+import java.io.Serializable;
+
+
+public class Song implements Serializable{
+    /**
+     * 歌手
+     */
+    public String singer;
+    /**
+     * 歌曲名
+     */
+    public String song;
+    /**
+     * 歌曲的地址
+     */
+    public String path;
+    /**
+     * 歌曲长度
+     */
+    public int duration;
+    /**
+     * 歌曲的大小
+     */
+    public long size;
+}
